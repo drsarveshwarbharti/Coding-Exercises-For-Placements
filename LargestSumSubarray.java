3419cef8ed4e1372705507029c6399a6ba83dce0
Dr. Sarveshwar Bharti <sarveshwar.csc@gndu.ac.in>
2025-08-20 12:50:26 +0530

    Update LargestSumSubarray.java
    Added easy understandable logic inside for loop using if else for currentSum and maxSum variables

--- LargestSumSubarray.java
+++ LargestSumSubarray.java
@@ -32,16 +32,34 @@ class LargestSumSubarray {
             // Either extend the subarray or start a new one
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
+         
+         //=====This is the easy understandable logic=====
+         /*
+         //CurrentSum
+         if(nums[i] > currentSum + nums[i]){
+                currentSum = nums[i];
+         }else{
+                currentSum = currentSum + nums[i];
+         }
+         //maxSum
+         if(currentSum > maxSum){
+                maxSum = currentSum;
+         }
+         */
+         //============================================
         }
         return maxSum;
     }
 public static void main(String[] args) {
-       // int[] nums1 = {-2,1,-3,4,-1,2,1,-5,4};
+        //Test Case-1
+        // int[] nums1 = {-2,1,-3,4,-1,2,1,-5,4};
         //System.out.println("Output: " + maxSubArray(nums1));  // 6
-        
+
+        //Test Case-2
         //int[] nums2 = {1};
         //System.out.println("Output: " + maxSubArray(nums2));  // 1
 
+        //Test Case-3
         int[] nums3 = {5,4,-1,7,8};
         System.out.println("Output: " + maxSubArray(nums3));  // 23
     }
